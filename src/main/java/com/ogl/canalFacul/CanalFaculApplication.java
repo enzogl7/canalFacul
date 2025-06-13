42e2449bdcdc3185185e14502aced0afb1b48f74
enzogl7 <enzolima527@gmail.com>
2025-06-13 14:12:23 -0300

    create spring project

---
+++ src/main/java/com/ogl/canalFacul/CanalFaculApplication.java
@@ -0,0 +1,13 @@
+package com.ogl.canalFacul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalFaculApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanalFaculApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ogl/canalFacul/infra/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ogl.canalFacul.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/resources/**", "/assets/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/").permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
